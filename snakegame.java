1e71087ceb8b76f9a321e12eb3158b6d74afc9a6
shrey-creator <60284082+shrey-creator@users.noreply.github.com>
2020-05-10 12:38:31 +0530

    Add files via upload

--- snakegame.java
+++ snakegame.java
@@ -1,36 +1,46 @@
 package snake2;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class snakegame extends JFrame implements  KeyListener
+public class snakegame extends JFrame implements  KeyListener, ActionListener
 {
-   int count=0;
-   int store=0;
-   boolean whole;
-   String head;
-     int xdir=0;            //to check if snake is facing east or west
-     int ydir=0;            //to check if snake is facing north or south
-    static int arrayx[];        //to store x point of the snake
-    static int arrayy[];            //to store y point of the snake
-    public snakegame()
-    {
-        head="right";
-        addKeyListener(this);
 
-        arrayx=new int[10];
-        arrayy =new int[10];
-        for(int i=0;i<arrayx.length;i++)
-        {
-            arrayx[i] = 250;
-            arrayy[i] = 250;
+   String head;
+     int xdir=0;
+
+     static ArrayList<Integer>  arrayx;      //to store x point of the snake
+     static ArrayList<Integer> arrayy;//to store y point of the snake
+    Timer timer;
+    int delay=100;
+    public snakegame() {
+        head = "right";
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer=new Timer(delay,this);
+        timer.start();
+        arrayx = new ArrayList<Integer>();
+        arrayy =  new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            arrayx.add(250-5*i);
+
         }
+        for (int i = 0; i < 10; i++)
+        {
+            arrayy.add(250);
+        }
+
 
     }
 
     public static void main(String args[])
     {
+
         snakegame jf=new snakegame();
         JFrame j=new JFrame();
         jf.setTitle("Snake game");
@@ -44,11 +54,13 @@ public class snakegame extends JFrame implements  KeyListener
     {
         g.setColor(Color.BLACK);
         g.fillRect(0,0,500,500);
-        for(int i=0;i<arrayx.length;i++)
+        System.out.println(arrayx.size());
+
+        for(int i=0;i<arrayx.size();i++)
         {
 
             g.setColor(Color.white);
-            g.fillRect((arrayx[i]-(5*i)),arrayy[i], 5, 5);
+            g.fillRect(arrayx.get(i),arrayy.get(i), 5, 5);
         }
 
     }
@@ -63,139 +75,61 @@ public class snakegame extends JFrame implements  KeyListener
     {
             if(k.getKeyCode() == KeyEvent.VK_RIGHT && !head.equals("left"))
             {
+                    //
+                    for (int i = arrayx.size() - 1; i >= 1; i--) {
 
-                if(!(head.equals("right"))) {
-                     store=count;
-                    count = 0;
-                }
-                 if(head.equals("up") )
-                    xdir=0;
-
-                //when snake is already moving in right(same) direction
-                if(xdir>0 && head.equals("right"))
-                { System.out.println("hi");
-                    for(int i=0;i<arrayx.length;i++) {
-                    arrayx[i] += 5;
-
-                }
-
-                }
-                //when snake is not in right direction
-                else {
-                    //REST BODY OF SNAKE
-                    if (ydir>0 || head.equals("up")|| head.equals("right") )
-                    {
-                        for(int i=count+1;i<arrayx.length;i++)
-                        arrayy[i] -= 5;             //snake is in north direction
+                        arrayx.set(i, arrayx.get(i - 1));
                     }
-                    else if( ydir<0 || head.equals("down") )
-                    {
-                        for(int i=count+1;i<arrayx.length;i++)
-                        arrayy[i]+=5;           //snake is in south direction
+                    for (int i = arrayy.size() - 1; i >= 1; i--) {
+                        arrayy.set(i, arrayy.get(i - 1));
                     }
-;
-                    //HEAD OF THE SNAKE
-                    for(int i=0;i<=count;i++) {
+                    //head of the snake
+                    arrayx.set(0, arrayx.get(0) + 5);
+                    head = "right";
 
-                        arrayx[i] += 5;
-                    }
-                    count++;
-                    if(count==store)
-                    {
-                        count=0;
-                        xdir=1;
-                        ydir=0;
 
-                    }
-                }
-                System.out.println(xdir);
-                head="right";
+
 
 
             }
         else if(k.getKeyCode() == KeyEvent.VK_LEFT && !head.equals("right"))
         {
-            if(!(head.equals("left"))) {
-                store=count;
-                count = 0;
-            }
-
-            //when snake is already moving in left(same) direction
-
-            if(xdir<0 && head.equals("left"))
-            {
-                for(int i=0;i<arrayx.length;i++)
-                    arrayx[i] -= 5;
 
 
-            }
-            //when snake is not moving in left direction
-            else {
-                //BODY OF THE SNAKE
-                if ( ydir>0 || head.equals("down") || head.equals("left")) {
-                    for(int i=count+1;i<arrayx.length;i++)
-                        arrayy[i] += 5;         //snake is facing south
+                for (int i = arrayx.size() - 1; i >= 1; i--) {
+
+                    arrayx.set(i, arrayx.get(i - 1));
                 }
-                else if(ydir<0 || head.equals("up"))
-                {
-                    for(int i=count+1;i<arrayx.length;i++)
-                        arrayy[i]-=5;           //snake is facing north
+                for (int i = arrayy.size() - 1; i >= 1; i--) {
+                    arrayy.set(i, arrayy.get(i - 1));
                 }
-                //Head of the snake
-                for(int i=0;i<=count;i++)
-                  arrayx[i] -= 5;
-                count++;
-                if(count==store)
-                {
-                    count=0;
-                    xdir=-1;
-                    ydir=0;
+                //head of the snake
 
-                }
+                arrayx.set(0, arrayx.get(0) - 5);
 
-            }
-            head="left";
+                head = "left";
+
+
 
         }
         else if(k.getKeyCode() == KeyEvent.VK_UP && !head.equals("down"))        //UP DIRECTION
         {
-            if(!(head.equals("up"))) {
-                store=count;
-                count = 0;
+
+
+            for(int i=arrayx.size()-1;i>=1;i--) {
+
+                arrayx.set(i,arrayx.get(i-1));
             }
-                //if snake is moving in the same direction
-            if(ydir>0 && head.equals("up"))
+            for(int i=arrayy.size()-1;i>=1;i--)
             {
-                for(int i=0;i<arrayx.length;i++) {
-                    arrayy[i] -= 5;
-
-                }
+                arrayy.set(i,arrayy.get(i-1));
             }
-            //if snake is not moving in the same direction
-            else {
-                //BODY OF THE SNAKE
-                if (xdir>0 || head.equals("right") || head.equals("up")) {
-                    for(int i=count+1;i<arrayx.length;i++)
-                    arrayx[i] += 5;         //snake is facing right
-                }
-                else if ( xdir<0 || head.equals("left")) {
-                    for(int i=count+1;i<arrayx.length;i++)
-                    arrayx[i] -= 5;         //snake is facing left
-                }
-                //HEAD OF THE SNAKE
-                for(int i=0;i<=count;i++) {
-                    arrayy[i] -= 5;
-                }
+            //head of the snake
 
-                count++;
-                if(count==store)
-                {
-                    count=0;
-                    xdir=0;
-                    ydir=1;
-                }
-            }
+            arrayy.set(0,arrayy.get(0)-5);
             head="up";
+
+
 
 
 
@@ -203,46 +137,21 @@ public class snakegame extends JFrame implements  KeyListener
         }
         else if(k.getKeyCode() == KeyEvent.VK_DOWN && !head.equals("up"))      //DOWN DIRECTION
         {
-            if(!(head.equals("down"))) {
-                store=count;
-                count = 0;
+
+            for(int i=arrayx.size()-1;i>=1;i--) {
+
+                arrayx.set(i,arrayx.get(i-1));
             }
-            if(head.equals("right"))
-                ydir=0;
-
-
-            //if snake is moving in same(down) direction
-            if(ydir<0 && head.equals("down"))
+            for(int i=arrayy.size()-1;i>=1;i--)
             {
-                for(int i=0;i<arrayx.length;i++)
-                    arrayy[i]+=5;
-
-
+                arrayy.set(i,arrayy.get(i-1));
             }
-            //if snake is not moving in same direction(Down) direction
-            else {
-                    //BODY OF THE SNAKE
-                if ( xdir>0  || head.equals("down") || head.equals("right")) {
-                    for(int i=count+1;i<arrayx.length;i++)//FOR HEAD OF SNAKE
-                    arrayx[i] += 5;     //down when snake is facing right
-                }
-                else if(xdir<0 || head.equals("left")) {
-                    for(int i=count+1;i<arrayx.length;i++)
-                        arrayx[i] -= 5;
-                }//down when snake is facing left
-                //HEAD OF THE SNAKE
-                for(int i=0;i<=count;i++)
-                 arrayy[i] += 5;
-                count++;
-                if(count==store)
-                {
-                    count=0;
-                    xdir=0;
-                    ydir=-1;
+            //head of the snake
 
-                }
-            }
+            arrayy.set(0,arrayy.get(0)+5);
             head="down";
+
+
 
 
 
@@ -259,4 +168,11 @@ public class snakegame extends JFrame implements  KeyListener
     }
 
 
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        timer.start();
+
+    }
 }
